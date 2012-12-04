17c4b0e31be48ca6ebc4d2f1611b3221a421080a
Mike Sherov <mike.sherov@gmail.com>
2012-12-04 14:33:54 -0500

    add keyCode and buttonCode constants

--- jquery.simulate.js
+++ jquery.simulate.js
@@ -33,6 +33,40 @@ $.simulate = function( elem, type, options ) {
 	}
 };
 
+$.extend( $.simulate, {
+
+	keyCode: {
+		BACKSPACE: 8,
+		COMMA: 188,
+		DELETE: 46,
+		DOWN: 40,
+		END: 35,
+		ENTER: 13,
+		ESCAPE: 27,
+		HOME: 36,
+		LEFT: 37,
+		NUMPAD_ADD: 107,
+		NUMPAD_DECIMAL: 110,
+		NUMPAD_DIVIDE: 111,
+		NUMPAD_ENTER: 108,
+		NUMPAD_MULTIPLY: 106,
+		NUMPAD_SUBTRACT: 109,
+		PAGE_DOWN: 34,
+		PAGE_UP: 33,
+		PERIOD: 190,
+		RIGHT: 39,
+		SPACE: 32,
+		TAB: 9,
+		UP: 38
+	},
+
+	buttonCode: {
+		LEFT: 0,
+		MIDDLE: 1,
+		RIGHT: 2
+	}
+});
+
 $.extend( $.simulate.prototype, {
 
 	simulateEvent: function( elem, type, options ) {
