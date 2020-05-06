46c75ebf2584124d2ac41dae2cd613149287c2fe
Michał Gołębiowski-Owczarek <m.goleb@gmail.com>
2020-05-06 17:18:05 +0200

    Update dependencies, make the build work with Node 12, add missing jQueries
    Closes #52

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,29 +30,50 @@ grunt.initConfig({
 				"qunit/qunit.css": "qunit/qunit/qunit.css",
 				"qunit/LICENSE.txt": "qunit/LICENSE.txt",
 
-				"jquery-1.6.0/jquery.js": "jquery-1.6.0/jquery.js",
-				"jquery-1.6.0/MIT-LICENSE.txt": "jquery-1.6.0/MIT-LICENSE.txt",
-
-				"jquery-1.6.1/jquery.js": "jquery-1.6.1/jquery.js",
-				"jquery-1.6.1/MIT-LICENSE.txt": "jquery-1.6.1/MIT-LICENSE.txt",
-
-				"jquery-1.6.2/jquery.js": "jquery-1.6.2/jquery.js",
-				"jquery-1.6.2/MIT-LICENSE.txt": "jquery-1.6.2/MIT-LICENSE.txt",
-
-				"jquery-1.6.3/jquery.js": "jquery-1.6.3/jquery.js",
-				"jquery-1.6.3/MIT-LICENSE.txt": "jquery-1.6.3/MIT-LICENSE.txt",
-
-				"jquery-1.6.4/jquery.js": "jquery-1.6.4/jquery.js",
-				"jquery-1.6.4/MIT-LICENSE.txt": "jquery-1.6.4/MIT-LICENSE.txt",
-
-				"jquery-1.7.0/jquery.js": "jquery-1.7.0/jquery.js",
-				"jquery-1.7.0/MIT-LICENSE.txt": "jquery-1.7.0/MIT-LICENSE.txt",
-
-				"jquery-1.7.1/jquery.js": "jquery-1.7.1/jquery.js",
-				"jquery-1.7.1/MIT-LICENSE.txt": "jquery-1.7.1/MIT-LICENSE.txt",
-
 				"jquery-1.7.2/jquery.js": "jquery-1.7.2/jquery.js",
-				"jquery-1.7.2/MIT-LICENSE.txt": "jquery-1.7.2/MIT-LICENSE.txt"
+				"jquery-1.7.2/MIT-LICENSE.txt": "jquery-1.7.2/MIT-LICENSE.txt",
+
+				"jquery-1.8.3/jquery.js": "jquery-1.8.3/jquery.js",
+				"jquery-1.8.3/MIT-LICENSE.txt": "jquery-1.8.3/MIT-LICENSE.txt",
+
+				"jquery-1.9.1/jquery.js": "jquery-1.9.1/jquery.js",
+				"jquery-1.9.1/MIT-LICENSE.txt": "jquery-1.9.1/MIT-LICENSE.txt",
+
+				"jquery-1.10.2/jquery.js": "jquery-1.10.2/jquery.js",
+				"jquery-1.10.2/MIT-LICENSE.txt": "jquery-1.10.2/MIT-LICENSE.txt",
+
+				"jquery-1.11.3/jquery.js": "jquery-1.11.3/dist/jquery.js",
+				"jquery-1.11.3/MIT-LICENSE.txt": "jquery-1.11.3/MIT-LICENSE.txt",
+
+				"jquery-1.12.4/jquery.js": "jquery-1.12.4/dist/jquery.js",
+				"jquery-1.12.4/LICENSE.txt": "jquery-1.12.4/LICENSE.txt",
+
+				"jquery-2.0.3/jquery.js": "jquery-2.0.3/jquery.js",
+				"jquery-2.0.3/MIT-LICENSE.txt": "jquery-2.0.3/MIT-LICENSE.txt",
+
+				"jquery-2.1.4/jquery.js": "jquery-2.1.4/dist/jquery.js",
+				"jquery-2.1.4/MIT-LICENSE.txt": "jquery-2.1.4/MIT-LICENSE.txt",
+
+				"jquery-2.2.4/jquery.js": "jquery-2.2.4/dist/jquery.js",
+				"jquery-2.2.4/LICENSE.txt": "jquery-2.2.4/LICENSE.txt",
+
+				"jquery-3.0.0/jquery.js": "jquery-3.0.0/dist/jquery.js",
+				"jquery-3.0.0/LICENSE.txt": "jquery-3.0.0/LICENSE.txt",
+
+				"jquery-3.1.1/jquery.js": "jquery-3.1.1/dist/jquery.js",
+				"jquery-3.1.1/LICENSE.txt": "jquery-3.1.1/LICENSE.txt",
+
+				"jquery-3.2.1/jquery.js": "jquery-3.2.1/dist/jquery.js",
+				"jquery-3.2.1/LICENSE.txt": "jquery-3.2.1/LICENSE.txt",
+
+				"jquery-3.3.1/jquery.js": "jquery-3.3.1/dist/jquery.js",
+				"jquery-3.3.1/LICENSE.txt": "jquery-3.3.1/LICENSE.txt",
+
+				"jquery-3.4.1/jquery.js": "jquery-3.4.1/dist/jquery.js",
+				"jquery-3.4.1/LICENSE.txt": "jquery-3.4.1/LICENSE.txt",
+
+				"jquery-3.5.1/jquery.js": "jquery-3.5.1/dist/jquery.js",
+				"jquery-3.5.1/LICENSE.txt": "jquery-3.5.1/LICENSE.txt"
 			}
 		}
 	},
